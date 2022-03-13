0fc20e12164125eadca276bf56ccc4a9ae8313d3
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-13 15:15:31 +1100

    async testing of the 'terms and conditions' popover

--- src/pages/summary/SummaryForm.test.jsx
+++ src/pages/summary/SummaryForm.test.jsx
@@ -1,6 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "./SummaryForm";
-import userEvent from "@testing-library/user-event"
+import userEvent from "@testing-library/user-event";
 
 test("Initial conditions", () => {
   render(<SummaryForm />);
@@ -13,35 +17,39 @@ test("Initial conditions", () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test('Checkbox disables button on first click and enables on second click', () => {
-    render (<SummaryForm />)
-    const checkbox = screen.getByRole("checkbox", {
-        name: /terms and conditions/i
-    })
-    const confirmButton = screen.getByRole("button", { name: /confirm order/i })
+test("Checkbox disables button on first click and enables on second click", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
 
-    userEvent.click(checkbox)
-    expect(confirmButton).toBeEnabled()
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
 
-    userEvent.click(checkbox)
-    expect(confirmButton).toBeDisabled()
-})
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
 
-test('popover response to hover', () => {
-    render (<SummaryForm />)
-    // popover starts out hidden
-    const nullPopover =screen.queryByText(/no ice cream will actually be delivered/i)
-    expect(nullPopover).not.toBeInTheDocument()
+test("popover response to hover", async () => {
+  render(<SummaryForm />);
+  // popover starts out hidden => Async
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
 
-    // popover appears upon mouseover of checkbox label
-    const termsAndConditions = screen.getByText(/terms and conditions/i)
-    userEvent.hover(termsAndConditions)
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
 
-    const popover = screen.getByText(/no ice cream will actually be delivered/i)
-    expect(popover).toBeInTheDocument()
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
 
-    // popover disappears when we mouse out
-    userEvent.unhover(termsAndConditions)
-    const nullPopoverAgain = screen.queryByText(/no ice cream will actually be delivered/i)
-    expect(nullPopoverAgain).not.toBeInTheDocument()
-})
+  // popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+  // eslint-disable-next-line testing-library/await-async-utils
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
+});
